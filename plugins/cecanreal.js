b0a97abf2c98632c30f78ad71133607aa9ae8a64
kyyyyasuuuuu <103046411+kyyyyasuuuuu@users.noreply.github.com>
2022-07-08 09:06:34 +0700

    Update cecanreal.js

--- plugins/cecanreal.js
+++ plugins/cecanreal.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, ('https://api.zacros.my.id/asupan/cecan'), '✿𝗖𝗥𝗘𝗔𝗧𝗢𝗥➢𝐆𝐞𝐦𝐩𝐲 𝐓𝐨𝐧\nSubscribe : https://www.youtube.com/channel/UCZoVmApPxtLYgUWwBD4nbCw', wm, 'NEXT', '.cevanvietnam', m)
+    conn.sendButtonImg(m.chat, ('https://api.zacros.my.id/asupan/cecan'), '✿𝗖𝗥𝗘𝗔𝗧𝗢𝗥➢𝗜𝗸𝘆𝗶𝗸𝘆\nFollow : https://www.instagram.com/riskysatyr', wm, 'NEXT', '.cevanvietnam', m)
 }
 
 handler.help = ['cecansantuy']
