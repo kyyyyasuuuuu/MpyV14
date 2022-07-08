7b1129112e21a96fe78c2dec6d07b3319c57f0a9
kyyyyasuuuuu <103046411+kyyyyasuuuuu@users.noreply.github.com>
2022-07-08 09:08:06 +0700

    Update cecanviet.js

--- plugins/cecanviet.js
+++ plugins/cecanviet.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, ('https://api.zacros.my.id/asupan/vietnam'), '✿𝗖𝗥𝗘𝗔𝗧𝗢𝗥➢𝐆𝐞𝐦𝐩𝐲 𝐓𝐨𝐧\nSubscribe : https://www.youtube.com/channel/UCZoVmApPxtLYgUWwBD4nbCw', wm, 'NEXT', '.cecanvietnam', m)
+    conn.sendButtonImg(m.chat, ('https://api.zacros.my.id/asupan/vietnam'), '✿𝗖𝗥𝗘𝗔𝗧𝗢𝗥➢𝗜𝗸𝘆𝗶𝗸𝘆\nFollow : https://www.instagram.com/riskysatyr', wm, 'NEXT', '.cecanvietnam', m)
 }
 
 handler.help = ['cecanvietnam']
